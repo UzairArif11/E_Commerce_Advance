4b1e832bbaf7a1bc20fa9ee68db8807066436006
uzair <uzairarifkhan1@gmail.com>
2025-04-07 19:18:47 +0500

    init

---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}", // ✅ Important for Vite + React
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
